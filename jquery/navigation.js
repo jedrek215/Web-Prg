6ce24864cb5d0e59896f246a76f2e1cfddd01dbb
Mary Louise <marylouiseparagas@gmail.com>
2017-03-26 16:51:57 +0800

    dry-run

--- jquery/navigation.js
+++ jquery/navigation.js
@@ -1,13 +1,13 @@
  $(function() {
             // get current URL path and assign 'active' class
-            console.log(window.location.pathname);
-            var pathname = window.location.pathname;
+            console.log(window.location.href);
+            var pathname = window.location.href;
             $('#menuBar > li > a[href="'+pathname+'"]').parent().addClass('active');
         });
  
   $(function() {
             // get current URL path and assign 'active' class
-            console.log(window.location.pathname);
-            var pathname = window.location.pathname;
+            console.log(window.location.href);
+            var pathname = window.location.href;
             $('.nav.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
         });
