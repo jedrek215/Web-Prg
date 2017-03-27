0b65fcbbeab2406d214cf083fbdc16692a988d7c
Mary Louise <marylouiseparagas@gmail.com>
2017-03-28 06:59:50 +0800

    all

--- application/controllers/Class_Cont.php
+++ application/controllers/Class_Cont.php
@@ -2,16 +2,28 @@
 
 class Class_Cont extends CI_Controller{
 
-	
-	public function index()
+	function __construct(){
+
+        parent::__construct();
+        $this->load->model('Class_Model');
+       //$this->load->model('Arts_Model');
+		//$this->load->model('dash_model', 'model');
+    }
+
+	public function index($department = NULL)
 	{
-		$this->home();
+		if(!$department){
+			$this->home();
+		}
 	}
 
 	public function home(){
+
+		$data['nav'] = $this->Class_Model->getCollege();
+		$data['dept'] = $this->Class_Model->getDepartment()
 		$this->load->view('include/classes_head');
 		$this->load->view('include/top_nav');
-		$this->load->view('include/department_nav');
+		$this->load->view('include/department_nav', $data);
 		$this->load->view('classes');
 		$this->load->view('include/modal');
 
