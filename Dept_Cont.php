a2dca26557c0bb42fe00159160eecd3848750f64
Mary Louise <marylouiseparagas@gmail.com>
2017-04-02 01:11:54 +0800

    added session

---
+++ Dept_Cont.php
@@ -0,0 +1,42 @@
+<?php
+
+class Dept_Cont extends CI_Controller{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Home_model');
+		$this->load->model("Class_model", "menu");
+	}
+
+	public function index()
+	{
+		$this->home();
+	}
+
+	public function home(){	
+		if ($this->session->userdata('logged_in')){
+	      $session_data = $this->session->userdata('logged_in');
+	      $data['username'] = $session_data['username'];
+		$data['college'] = $this->menu->getCollege();
+		$deptid = $this->uri->segment(3);
+		$data['deptname'] = $this->menu->getDeptName($deptid);
+		$data['classes'] = $this->menu->getClasses($deptid);
+		$data['userinfo'] = $this->Home_model->get_userId($data['username']);
+		$datestring = '%Y-%m-%d %H:%i:%s';
+        $time = time();
+        $data['timestamp'] = mdate($datestring, $time);	
+		$this->load->view('include/department_head');
+		$this->load->view('include/top_nav',$data);
+		$this->load->view('include/department_nav', $data);
+		$this->load->view('department',$data);
+		$this->load->view('include/modal',$data);
+	}
+	}
+
+
+	
+
+	
+}
+?>
