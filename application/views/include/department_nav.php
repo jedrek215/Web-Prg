091de174af21a5f82f3c69225492e75aceba9e05
Mary Louise <marylouiseparagas@gmail.com>
2017-04-02 00:31:53 +0800

    integrated php dynamic multii-level menu

--- application/views/include/department_nav.php
+++ application/views/include/department_nav.php
@@ -10,61 +10,18 @@
                           <div class="navbar-collapse collapse sidebar-navbar-collapse">
                           <ul class="nav navbar-nav">
                             <div id="colleges">
-                            <li><a href="#" data-toggle="collapse" data-target="#cobMenu" aria-expanded="false">Business <i class="fa fa-sort-desc" style="margin-left: 75px; color: gray;" aria-hidden="true"></i></a></li>
-                                    <ul class="nav collapse collMenu" id="cobMenu" role="menu" aria-labelledby="btn-1">
-                                        <li><a href="#">Accountancy</a></li>
-                                        <li><a href="#">Commercial Law</a></li>
-                                        <li><a href="#">Decision Sciences & Innovation Department</a></li>
-                                        <li><a href="#">Management of Financial Institutions</a></li>
-                                        <li><a href="#">Management and Organization Department</a></li>
-                                        <li><a href="#">Marketing Management</a></li>
+                            <?php foreach($college as $parent):?>
+                            <li> <a href="#" data-toggle="collapse" data-target="#<?=$parent->college_code?>" aria-expanded="false"><?=$parent->college_name?> <i class="fa fa-sort-desc <?=$parent->college_code?>" style=" color: gray;" aria-hidden="true"></i></a></li>
+
+                            <?php if(count($this->menu->getDepartment($parent->college_id))){?>
+                                    <ul class="nav collapse collMenu" id="<?=$parent->college_code?>" role="menu" aria-labelledby="btn-1">
+                                        <?php foreach($this->menu->getDepartment($parent->college_id) as $child) :
+                                        echo '<li>'.anchor('Dept_Cont/index/'.$child->dept_id, $child->department). '</a></li>';
+                                       endforeach;?>
                                     </ul>   
-                            <li><a href="#" data-toggle="collapse" data-target="#ccsMenu" aria-expanded="false">Computer Studies <i class="fa fa-sort-desc" style="margin-left: 20px; color: gray;" aria-hidden="true"></i> </a></li>
-                                <ul class="nav collapse collMenu" id="ccsMenu" role="menu" aria-labelledby="btn-1">
-                                        <li><a href="dept1.html">Computer Technology</a></li>
-                                        <li><a href="#">Information Technology</a></li>
-                                        <li><a href="#">Software Technology</a></li>
-                                </ul>
-                            <li><a href="#" data-toggle="collapse" data-target="#cedMenu" aria-expanded="false">Education<i class="fa fa-sort-desc" style="margin-left: 72px; color: gray;" aria-hidden="true"></i></a></li>
-                                <ul class="nav collapse collMenu" id="cedMenu" role="menu" aria-labelledby="btn-1">
-                                    <li><a href="#">Counseling and Educational Psychology Department</a></li>
-                                    <li><a href="#">Department of English and Applied Linguistics</a></li>
-                                    <li><a href="#">Educational Leadership and Management Department</a></li>
-                                    <li><a href="#">Physical Education Department</a></li>
-                                    <li><a href="#">Science Education Department</a></li>
-                                </ul>
-                            <li><a href="#" data-toggle="collapse" data-target="#coeMenu" aria-expanded="false">Engineering<i class="fa fa-sort-desc" style="margin-left: 60px; color: gray;" aria-hidden="true"></i></a></li>
-                                <ul class="nav collapse collMenu" id="coeMenu" role="menu" aria-labelledby="btn-1">
-                                    <li><a href="#">Chemical Engineering</a></li>
-                                    <li><a href="#">Civil Engineering</a></li>
-                                    <li><a href="#">Electronics and Communications Engineering</a></li>
-                                    <li><a href="#">Industrial Engineering</a></li>
-                                    <li><a href="#">Manufacturing Engineering and Management</a></li>
-                                    <li><a href="#">Mechanical Engineering</a></li>   
-                                </ul>
-                            <li><a href="#" style="padding-right:80px;">Law</a></li>
-                            <li><a href="#" data-toggle="collapse" data-target="#claMenu" aria-expanded="false">Liberal Arts<i class="fa fa-sort-desc" style="margin-left: 65px; color: gray;" aria-hidden="true"></i></a></li> 
-                                <ul class="nav collapse collMenu" id="claMenu" role="menu" aria-labelledby="btn-1">
-                                    <li><a href="#">Behavioral Sciences</a></li>
-                                    <li><a href="#">Communication</a></li>
-                                    <li><a href="#">Literature</a></li>
-                                    <li><a href="#">Filipino</a></li>
-                                    <li><a href="#">History</a></li>
-                                    <li><a href="#">International Studies</a></li>
-                                    <li><a href="#">Philosophy</a></li>
-                                    <li><a href="#">Political Science</a></li>
-                                    <li><a href="#">Psychology</a></li>
-                                    <li><a href="#">Theology and Religious Education</a></li>
-                                </ul>
-                            <li><a href="#" data-toggle="collapse" data-target="#cosMenu" aria-expanded="false">Science<i class="fa fa-sort-desc" style="margin-left: 85px; color: gray;" aria-hidden="true"></i></a></li>
-                                <ul class="nav collapse collMenu" id="cosMenu" role="menu" aria-labelledby="btn-1">
-                                    <li><a href="#">Biology</a></li>
-                                    <li><a href="#">Chemistry</a></li>
-                                    <li><a href="#">Mathematics</a></li>
-                                    <li><a href="#">Physics</a></li>
-                                </ul>
-                            <li><a href="#">Economics</a></li>
-                              </div>
+                             <?php };?>
+                             <?php endforeach;?>
+                            </div>
                           </ul>
                         </div>
                     </div>
