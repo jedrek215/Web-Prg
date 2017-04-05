e5ee3d9dc4fcaf4b5e1f9b8974b74f4abd3095ae
KyleMatthewC <kyle_matthew_chua@dlsu.edu.ph>
2017-04-06 01:35:11 +0800

    jquery

---
+++ jquery/button.js
@@ -0,0 +1,39 @@
+function btnOver(btn){
+	if(btn.innerText == 'Follow'){
+		btn.style.background = '#efefef';
+	}
+	else{
+		btn.style.background = '#efefef';
+		btn.style.color = '#f9a825';
+		btn.innerText = 'Unfollow'
+	}	
+};
+
+function btnOut(btn){
+	if(btn.innerText == 'Follow'){
+		btn.style.background = 'white';
+		btn.style.color = '#f9a825';
+
+		var url = "Home_Cont/getUserInfo";
+
+	}
+	else{
+		btn.style.background = '#f9a825';
+		btn.style.color = 'white';
+		btn.innerText = 'Following'
+	}	
+};
+
+base_url = '<?=base_url()?>';
+
+function threadBtnClick(btn, thread_id){
+	if(btn.innerText == 'Follow'){
+		btn.style.background = '#f9a825';
+		btn.style.color = 'white';
+		btn.innerText = 'Following'
+		
+		console.log(thread_id);
+	}
+}
+
+
